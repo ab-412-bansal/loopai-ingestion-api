4777043154a994a951ed213bc6a20658b6d91f0b
Ayush-412-Bansal <ayush4bansal@gmail.com>
2025-06-05 11:05:46 +0530

    Modified main.py for deployment

--- app/main.py
+++ app/main.py
@@ -43,5 +43,5 @@ def get_status(ingestion_id):
     })
 
 if __name__ == "__main__" or __name__ == "app.main":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
